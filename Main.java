075e65db7b6c1621f518f898dc6d0831a8122741
Lex <u16ak17@abdn.ac.uk>
2023-03-25 11:24:31 +0000

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Phonebook phonebook = new Phonebook();
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        while (true) {
+            System.out.println(
+                    "Select an option: n1. Add a contact, n2. Remove a contact, n3. Update a contact, n4. Search, n5. Display all contacts, n6. Quit");
+            choice = scanner.nextInt();
+            scanner.nextLine(); // to consume the leftover newline character
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter a name to add to the phonebook:");
+                    String name = scanner.nextLine();
+
+                    System.out.println("Enter a phone number for " + name + ":");
+                    String phoneNumber = scanner.nextLine();
+
+                    phonebook.addEntry(name, phoneNumber);
+
+                    System.out.println(name + " has been added to the phonebook.");
+                    break;
+                case 2:
+                    System.out.println("Enter a name to remove from the phonebook:");
+                    name = scanner.nextLine();
+
+                    phonebook.deleteEntry(name);
+
+                    System.out.println(name + " has been removed from the phonebook.");
+                    break;
+                case 3:
+                    System.out.println("Enter a name to update in the phonebook:");
+                    name = scanner.nextLine();
+
+                    System.out.println("Enter a new phone number for " + name + ":");
+                    phoneNumber = scanner.nextLine();
+
+                    phonebook.updateEntry(name, phoneNumber);
+
+                    System.out.println(name + "'s phone number has been updated in the phonebook.");
+                    break;
+                case 4:
+                    System.out.println(
+                            "Select a search method: n1. Search by exact name, n2. Search by exact phone number, n3. Search by part of a name or number");
+                    int searchChoice = scanner.nextInt();
+                    scanner.nextLine(); // to consume the leftover newline character
+
+                    switch (searchChoice) {
+                        case 1:
+                            System.out.println("Enter a name to search for:");
+                            name = scanner.nextLine();
+
+                            phonebook.getPhoneNumber(name);
+                            break;
+                        case 2:
+                            System.out.println("Enter a phone number to search for:");
+                            phoneNumber = scanner.nextLine();
+
+                            phonebook.getName(phoneNumber);
+                            break;
+                        case 3:
+                            System.out.println("Enter a part of a name or part of a phone number to search for:");
+                            String partialName = scanner.nextLine();
+
+                            phonebook.searchContacts(partialName);
+                            break;
+                        default:
+                            System.out.println("Invalid search method.");
+                            break;
+                    }
+                    break;
+                case 5:
+                    phonebook.displayAllContacts();
+                    break;
+                case 6:
+                    System.out.println("Exiting phonebook...");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Phonebook.java
@@ -0,0 +1,131 @@
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Phonebook {
+    private Connection connection;
+
+    public Phonebook() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String dbURL = "jdbc:sqlite:phonebook.sqlite";
+            connection = DriverManager.getConnection(dbURL);
+            if (connection != null) {
+                System.out.println("Connected to the database");
+                DatabaseMetaData dm = (DatabaseMetaData) connection.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+
+                String createTableSQL = "CREATE TABLE IF NOT EXISTS phonebook (name TEXT, phoneNumber TEXT)";
+                PreparedStatement preparedStatement = connection.prepareStatement(createTableSQL);
+                preparedStatement.execute();
+
+            }
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void addEntry(String name, String phoneNumber) {
+        try {
+            String addEntrySQL = "INSERT INTO phonebook (name, phoneNumber) VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(addEntrySQL);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, phoneNumber);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getPhoneNumber(String name) {
+        try {
+            String getPhoneNumberSQL = "SELECT phoneNumber FROM phonebook WHERE name = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(getPhoneNumberSQL);
+            preparedStatement.setString(1, name);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String phoneNumber = resultSet.getString("phoneNumber");
+                System.out.println(name + ": " + phoneNumber);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getName(String phoneNumber) {
+        try {
+            String getNameSQL = "SELECT name FROM phonebook WHERE phoneNumber = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(getNameSQL);
+            preparedStatement.setString(1, phoneNumber);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                System.out.println(name + ": " + phoneNumber);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void searchContacts(String query) {
+        try {
+            String searchSQL = "SELECT * FROM phonebook WHERE name LIKE ? or phoneNumber LIKE ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(searchSQL);
+            preparedStatement.setString(1, "%" + query + "%");
+            preparedStatement.setString(2, "%" + query + "%");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String phoneNumber = resultSet.getString("phoneNumber");
+                System.out.println(name + ": " + phoneNumber);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteEntry(String name) {
+        try {
+            String deleteEntrySQL = "DELETE FROM phonebook WHERE name = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(deleteEntrySQL);
+            preparedStatement.setString(1, name);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateEntry(String name, String newPhoneNumber) {
+        try {
+            String updateEntrySQL = "UPDATE phonebook SET phoneNumber = ? WHERE name = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(updateEntrySQL);
+            preparedStatement.setString(1, newPhoneNumber);
+            preparedStatement.setString(2, name);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayAllContacts() {
+        try {
+            String selectAllSQL = "SELECT * FROM phonebook";
+            PreparedStatement preparedStatement = connection.prepareStatement(selectAllSQL);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String phoneNumber = resultSet.getString("phoneNumber");
+                System.out.println(name + ": " + phoneNumber);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
